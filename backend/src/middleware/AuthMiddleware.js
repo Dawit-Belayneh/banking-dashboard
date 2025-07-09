ad4906ee2fad8fd2c80ea8c210c306c74de141e4
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-07-09 11:03:07 -0900

    update

--- backend/src/middleware/AuthMiddleware.js
+++ backend/src/middleware/AuthMiddleware.js
@@ -4,17 +4,18 @@ const JWTService = require("../utils/JwtService")
 const AuthMiddleware = (req,res,next)=>{
     try{
         const headers =req.headers['authorization'] ||''
-        if(!headers || !headers.startswith("Bearer ")){
+        if(!headers || !headers.startsWith("Bearer ")){
             throw new ApiError(401,"Please Login First")
         }
 
-        const token = headers.split(" ")(1)
-         if(token){
+        const token = headers.split(" ")[1]
+         if(!token){
             throw new ApiError(401,"Enter Valid Details")
         }
 
         const payload =JWTService.ValidateToken(token)
         req.user = payload.user
+        next()
 
 
     }catch (error) {

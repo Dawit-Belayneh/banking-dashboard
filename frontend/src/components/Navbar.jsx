6046f5643e6194f890426678da0f7ac223a6a445
Dawit Belayneh <dawitbelayneh9@gmail.com>
2025-07-19 11:31:24 -0900

    frontend update

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+    return(
+        <>
+        <header className="w-full border-b rounded-b-md">
+            <nav className="w-[98%] lg:w-[80%] mx-aouto py-3 flext items-center justify-between">
+                 
+            </nav>
+        </header>
+        </>
+    )
+}
+
+export default Navbar
